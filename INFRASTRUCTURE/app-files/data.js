44ea111d01f965cb4aa01f94681e2f894a8db9f6
Iky-12 <rifkimhmd610@gmail.com>
2025-04-21 22:46:53 +0700

    first commit

---
+++ INFRASTRUCTURE/app-files/data.js
@@ -0,0 +1,755 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-lapangan-tengah",
+      "name": "lapangan tengah",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "yaw": 2.4778137578604884,
+        "pitch": 0.2098723090206427,
+        "fov": 1.2812361540040014
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1926399457462775,
+          "pitch": 0.21567015735415396,
+          "rotation": 0,
+          "target": "1-xi-1"
+        },
+        {
+          "yaw": -3.1381528992362213,
+          "pitch": 0.2722861083219268,
+          "rotation": 0,
+          "target": "3-uks"
+        },
+        {
+          "yaw": -1.262093664059977,
+          "pitch": 0.2116658491412835,
+          "rotation": 0,
+          "target": "10-wakasek"
+        },
+        {
+          "yaw": -0.08964161651591596,
+          "pitch": 0.26696969779769475,
+          "rotation": 0,
+          "target": "8-perpustakaan"
+        },
+        {
+          "yaw": 1.0999104482196334,
+          "pitch": 0.32283821213498953,
+          "rotation": 5.497787143782138,
+          "target": "4-ruang-rapat"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-xi-1",
+      "name": "xi-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.183346945011035,
+          "pitch": 0.23334849113397915,
+          "rotation": 6.283185307179586,
+          "target": "0-lapangan-tengah"
+        },
+        {
+          "yaw": -2.9485275490901977,
+          "pitch": 0.2940530105347694,
+          "rotation": 0,
+          "target": "2-xi-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-xi-3",
+      "name": "xi-3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.19643968528595934,
+          "pitch": 0.3212890367412431,
+          "rotation": 0,
+          "target": "1-xi-1"
+        },
+        {
+          "yaw": -1.9369842441338854,
+          "pitch": 0.35043825068373735,
+          "rotation": 0.7853981633974483,
+          "target": "3-uks"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-uks",
+      "name": "uks",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8346619527593964,
+          "pitch": 0.2675787467608153,
+          "rotation": 0,
+          "target": "2-xi-3"
+        },
+        {
+          "yaw": 0.1773832242124911,
+          "pitch": 0.15014716277877582,
+          "rotation": 0,
+          "target": "0-lapangan-tengah"
+        },
+        {
+          "yaw": -1.2099982006536578,
+          "pitch": 0.16204837770553127,
+          "rotation": 0,
+          "target": "6-xi-5"
+        },
+        {
+          "yaw": -3.126612468286451,
+          "pitch": 0.1110969940000075,
+          "rotation": 0.7853981633974483,
+          "target": "12-indoor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-ruang-rapat",
+      "name": "ruang rapat",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.396078671222007,
+          "pitch": 0.1379135261751685,
+          "rotation": 0,
+          "target": "5-tangga"
+        },
+        {
+          "yaw": -1.1795911187014099,
+          "pitch": 0.2646503130567197,
+          "rotation": 0,
+          "target": "8-perpustakaan"
+        },
+        {
+          "yaw": 2.854837376977434,
+          "pitch": 0.1792298403689827,
+          "rotation": 0.7853981633974483,
+          "target": "1-xi-1"
+        },
+        {
+          "yaw": -2.3460344490020724,
+          "pitch": 0.20999181149473145,
+          "rotation": 0,
+          "target": "0-lapangan-tengah"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-tangga",
+      "name": "tangga",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.877006045105226,
+          "pitch": 0.5345083071632644,
+          "rotation": 0,
+          "target": "4-ruang-rapat"
+        },
+        {
+          "yaw": 2.2669199387794183,
+          "pitch": 0.05747745835155449,
+          "rotation": 0.7853981633974483,
+          "target": "17-xii-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-xi-5",
+      "name": "xi-5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "yaw": 1.385867990034237,
+        "pitch": 0.10681882917658925,
+        "fov": 1.2812361540040014
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.00854151851048,
+          "pitch": 0.24937372918054024,
+          "rotation": 0,
+          "target": "3-uks"
+        },
+        {
+          "yaw": 0.3197181364163182,
+          "pitch": 0.15612341238649563,
+          "rotation": 0,
+          "target": "10-wakasek"
+        },
+        {
+          "yaw": -1.082824691483891,
+          "pitch": 0.12808633202895336,
+          "rotation": 13.351768777756625,
+          "target": "11-kantin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-ruang-diskusi",
+      "name": "ruang diskusi",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0270831106236287,
+          "pitch": 0.12780370617714176,
+          "rotation": 0,
+          "target": "15-lab-biologi"
+        },
+        {
+          "yaw": 1.6745094251122241,
+          "pitch": 0.24720210412615273,
+          "rotation": 0,
+          "target": "17-xii-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-perpustakaan",
+      "name": "perpustakaan",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9319543352580517,
+          "pitch": 0.20566458082805816,
+          "rotation": 0,
+          "target": "10-wakasek"
+        },
+        {
+          "yaw": 1.0658932955892375,
+          "pitch": 0.12615619042230186,
+          "rotation": 0.7853981633974483,
+          "target": "4-ruang-rapat"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-xi-8",
+      "name": "xi-8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8401848243424013,
+          "pitch": 0.0713355507385085,
+          "rotation": 0.7853981633974483,
+          "target": "15-lab-biologi"
+        },
+        {
+          "yaw": -2.993778746045228,
+          "pitch": 0.2163490784629225,
+          "rotation": 13.351768777756625,
+          "target": "14-xii-10"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-wakasek",
+      "name": "wakasek",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6922067187560073,
+          "pitch": 0.1920711773143502,
+          "rotation": 0,
+          "target": "0-lapangan-tengah"
+        },
+        {
+          "yaw": -2.910094285814207,
+          "pitch": 0.22161994382393857,
+          "rotation": 0,
+          "target": "6-xi-5"
+        },
+        {
+          "yaw": 0.10062876742094318,
+          "pitch": 0.22909187073695847,
+          "rotation": 0,
+          "target": "8-perpustakaan"
+        },
+        {
+          "yaw": -1.1396530051137397,
+          "pitch": 0.1889449849124567,
+          "rotation": 5.497787143782138,
+          "target": "11-kantin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-kantin",
+      "name": "kantin",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7756742481544592,
+          "pitch": 0.17028543422571651,
+          "rotation": 0.7853981633974483,
+          "target": "10-wakasek"
+        },
+        {
+          "yaw": 2.916745831417292,
+          "pitch": 0.20679245599550455,
+          "rotation": 5.497787143782138,
+          "target": "6-xi-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-indoor",
+      "name": "indoor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.095341716144075,
+          "pitch": 0.25836036933640116,
+          "rotation": 5.497787143782138,
+          "target": "3-uks"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-masjid",
+      "name": "masjid",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8950901190636245,
+          "pitch": 0.12824110337435712,
+          "rotation": 0,
+          "target": "16-xii-7"
+        },
+        {
+          "yaw": 0.3872340845379103,
+          "pitch": 0.2544718458299009,
+          "rotation": 0,
+          "target": "15-lab-biologi"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-xii-10",
+      "name": "xii-10",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5338466308873757,
+          "pitch": 0.1375781172402082,
+          "rotation": 5.497787143782138,
+          "target": "9-xi-8"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-lab-biologi",
+      "name": "lab biologi",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9585554028540084,
+          "pitch": 0.19926029890599217,
+          "rotation": 0,
+          "target": "13-masjid"
+        },
+        {
+          "yaw": 0.23136908272173073,
+          "pitch": 0.28945511422228876,
+          "rotation": 0,
+          "target": "7-ruang-diskusi"
+        },
+        {
+          "yaw": -0.9204390767850743,
+          "pitch": 0.12370661550267492,
+          "rotation": 5.497787143782138,
+          "target": "9-xi-8"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-xii-7",
+      "name": "xii-7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1671546344937731,
+          "pitch": 0.20748797324981716,
+          "rotation": 0,
+          "target": "18-xii-5"
+        },
+        {
+          "yaw": -1.9116660793152693,
+          "pitch": 0.08374024501573452,
+          "rotation": 0.7853981633974483,
+          "target": "13-masjid"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "17-xii-1",
+      "name": "xii-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5665379192183746,
+          "pitch": 0.21113002611295784,
+          "rotation": 0,
+          "target": "18-xii-5"
+        },
+        {
+          "yaw": -0.9297489839804349,
+          "pitch": 0.21143092687551857,
+          "rotation": 0,
+          "target": "7-ruang-diskusi"
+        },
+        {
+          "yaw": 0.6083109714687609,
+          "pitch": 0.20836920025051775,
+          "rotation": 0,
+          "target": "5-tangga"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "18-xii-5",
+      "name": "xii-5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7918328352974857,
+          "pitch": 0.30794827044649153,
+          "rotation": 0,
+          "target": "16-xii-7"
+        },
+        {
+          "yaw": 0.17950742590175217,
+          "pitch": 0.37609362418975145,
+          "rotation": 0,
+          "target": "17-xii-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
